3bf6553bfb148b884284582042cd9f659430e89a
Animesh Mohanty <animeshmohanty59@gmail.com>
2025-12-02 21:40:20 +0530

    Add Sum_of_A_and_B.java to calculate sum of A and B
    Implemented a Java program to calculate the sum of two integers A and B.

---
+++ Day02_Variables&DataTypes/Sum_of_A_and_B.java
@@ -0,0 +1,14 @@
+//To find the sum of two numbers A and B
+
+public class Sum_of_A_and_B {
+    public static void main (String args []) {
+        int A = 15;
+        int B = 10;
+        int sum = A + B;
+        System.out.println("The sum of A and B is: " + sum);
+    }
+}
+
+
+// output
+// The sum of A and B is: 25
