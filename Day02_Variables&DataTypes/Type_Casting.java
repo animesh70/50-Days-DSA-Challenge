e4165e6395d1a44a186d791b4566b67a87e1b7e4
Animesh Mohanty <animeshmohanty59@gmail.com>
2025-12-02 22:40:57 +0530

    Add Type_Casting.java for float to int conversion
    Implement type casting from float to int in Type_Casting.java

---
+++ Day02_Variables&DataTypes/Type_Casting.java
@@ -0,0 +1,15 @@
+//Type Casting:
+
+import java.util.*;
+public class Type_Casting{
+  public static void main(String Args[]){
+    Scanner sc = new Scanner(System.in);
+    float a = sc.nextFloat();
+    int b = (int)a;
+    System.out.println(b);
+  }
+}
+
+// Output:
+// 25.12
+// 25
